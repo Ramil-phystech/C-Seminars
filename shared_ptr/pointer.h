654a1016933b629181400c74f5dfc23816b2f103
Ramil-phystech <fatkullin.rr@phystech.edu>
2024-11-09 10:46:20 +0300

    Update pointer.h

--- shared_ptr/pointer.h
+++ shared_ptr/pointer.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <functional>
 
 template<class T>
 class Shared_ptr {
@@ -7,44 +6,78 @@ class Shared_ptr {
     size_t *reference_count = nullptr;
 
 public:
-    Shared_ptr(const T &obj) {
-        pointer = new T(obj);
-        reference_count = new size_t(1);
-    }
+    Shared_ptr() {
+    };
 
-    Shared_ptr(const Shared_ptr<T> &rhs) {
-        pointer = rhs.pointer;
-        reference_count = rhs.reference_count;
-        ++(*reference_count);
-    }
+    explicit Shared_ptr(const T &obj);
 
-    ~Shared_ptr() {
-        --(*reference_count);
-        if (!(*reference_count)) {
-            delete pointer;
-            delete reference_count;
-        }
-    }
+    explicit Shared_ptr(T *const pobj);
 
-    Shared_ptr<T> &operator=(const Shared_ptr<T> &rhs) {
-        if (this == &rhs) {
-            return *this;
-        }
-        --(*reference_count);
-        if (!(*reference_count)) {
-            delete pointer;
-            delete reference_count;
-        }
-        reference_count = rhs.reference_count;
-        pointer = rhs.pointer;
-        ++(*reference_count);
-    }
+    Shared_ptr(const Shared_ptr<T> &rhs);
 
-    T& operator*() {
-        return *pointer;
-    }
+    ~Shared_ptr();
 
-    T* operator->() {
-        return pointer;
-    }
+    T &operator*() { return *pointer; }
+
+    Shared_ptr<T> &operator=(const Shared_ptr<T> &rhs);
+
+    size_t GetRefCount() const { return *reference_count; }
+    T *operator->() { return pointer; }
 };
+
+
+template<class T>
+Shared_ptr<T>::Shared_ptr(T *const pobj) {
+    pointer = pobj;
+    reference_count = new size_t(1);
+}
+
+template<class T>
+Shared_ptr<T>::Shared_ptr(const T &obj) {
+    pointer = new T(obj);
+    reference_count = new size_t(1);
+}
+
+template<class T>
+Shared_ptr<T>::Shared_ptr(const Shared_ptr<T> &rhs) {
+    pointer = rhs.pointer;
+    reference_count = rhs.reference_count;
+    ++(*reference_count);
+}
+
+template<class T>
+Shared_ptr<T>::~Shared_ptr() {
+    if (reference_count) {
+        --(*reference_count);
+        if (*reference_count == 0) {
+            delete reference_count;
+            delete pointer;
+        }
+    }
+}
+
+template<class T>
+Shared_ptr<T> &Shared_ptr<T>::operator=(const Shared_ptr<T> &rhs) {
+    if (this == &rhs) {
+        return *this;
+    }
+    if (reference_count) {
+        --(*reference_count);
+        if (*reference_count) {
+            delete reference_count;
+            delete pointer;
+        }
+    }
+    reference_count = rhs.reference_count;
+    pointer = rhs.pointer;
+    if (reference_count) {
+        ++(*reference_count);
+    }
+    return *this;
+}
+
+template<class T, class... Args>
+Shared_ptr<T> MakeShared(Args... args) {
+    auto *pointer = new T(args...);
+    return Shared_ptr<T>(pointer);
+}
